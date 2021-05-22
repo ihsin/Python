d0439d88516586dc484891249b48f202446faebd
Rahul Ranjan Sinha <luharihsin@gmail.com>
2021-05-22 18:41:06 +0530

    checks path and displays filename

---
+++ checkPath.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import getopt
+
+
+def checkPath():
+	"""checkPath.py -p <argument>"""
+	argv = sys.argv[1:]
+	path = os.getenv('HOME')
+	try:
+		opts, args = getopt.getopt(argv, "hp:")
+	except getopt.GetoptError:
+		print "checkPath.py -p <argument>"
+		sys.exit(1)
+	for opt, arg in opts:
+		if opt == '-h':
+			print "checkPath.py -p <argument>"
+			sys.exit()
+		if opt == '-p':
+			path = arg
+	if os.path.exists(path):
+		if os.path.isfile(path):
+			print "Name of the file is {} and directory is {}".format(filename, os.path.dirname(path))	
+
+checkPath()
+
